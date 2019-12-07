cc4035780be2aefbc25533ec28d3af02232157a4
Disassembler <disassembler@dasm.cz>
2019-12-07 11:45:37 +0100

    Rework country ip blocks source to MaxMind GeoLite2 ipdeny, ipverse and RIPE NCC are inaccurate

--- restrict-country/country.py
+++ restrict-country/country.py
@@ -1,21 +1,12 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-import requests
-
 country = 'cz'
-source = 'http://www.ipdeny.com/ipblocks/data/aggregated/{}-aggregated.zone'.format(country)
+source = '/var/www/html/ipblocks/{}.ipv4.collapsed'.format(country)
 destination = '/var/www/html/country-{}.rsc'.format(country)
 
-ips = []
-try:
-    r = requests.get(source)
-    for line in r.text.splitlines():
-        ips.append(line)
-    with open(destination, 'w') as f:
-        f.write('/ip firewall address-list\n')
-        f.write('remove [find list=country-{}]\n'.format(country))
-        for ip in sorted(set(ips)):
-            f.write('add list=country-{} address={}\n'.format(country, ip))
-except:
-    pass
+with open(source) as src, open(destination, 'w') as dst:
+    dst.write('/ip firewall address-list\n')
+    dst.write('remove [find list=CZ.zone]\n')
+    for block in src.readlines():
+        dst.write('add list=CZ.zone address={}'.format(block))
---
+++ restrict-country/country_ip_blocks.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import csv
+import io
+import ipaddress
+import os
+import requests
+import zipfile
+
+DST_DIR = '/var/www/html/ipblocks'
+
+# Dictionary of (name, ipv4, ipv6) tuples with geoname_id as key
+countries = {'': ('unknown', [], [])}
+
+with requests.Session() as session:
+    # Download the zipped CSVs
+    r = session.get('https://geolite.maxmind.com/download/geoip/database/GeoLite2-Country-CSV.zip')
+
+    # Unpack the zip in memory
+    with zipfile.ZipFile(io.BytesIO(r.content)) as zip:
+        date = zip.namelist()[0][21:29]
+
+        # Parse countries CSV
+        with io.BytesIO(zip.read('GeoLite2-Country-CSV_{}/GeoLite2-Country-Locations-en.csv'.format(date))) as f:
+            reader = csv.reader(io.TextIOWrapper(f))
+            next(reader, None) # Skip header
+            for row in reader:
+                country_code = row[4] if row[4] else row[2]
+                countries[row[0]] = (country_code.lower(), [], [])
+
+        # Parse IPv4 blocks CSV
+        with io.BytesIO(zip.read('GeoLite2-Country-CSV_{}/GeoLite2-Country-Blocks-IPv4.csv'.format(date))) as f:
+            reader = csv.reader(io.TextIOWrapper(f))
+            next(reader, None)
+            for row in reader:
+                geoname_id = row[2] if row[2] else row[1]
+                countries[geoname_id][1].append(row[0])
+
+        # Parse IPv6 blocks CSV
+        with io.BytesIO(zip.read('GeoLite2-Country-CSV_{}/GeoLite2-Country-Blocks-IPv6.csv'.format(date))) as f:
+            reader = csv.reader(io.TextIOWrapper(f))
+            next(reader, None)
+            for row in reader:
+                geoname_id = row[2] if row[2] else row[1]
+                countries[geoname_id][2].append(row[0])
+
+for country in countries.values():
+    country_code = country[0]
+
+    # Write IPv4 blocks file per country
+    with open(os.path.join(DST_DIR, '{}.ipv4.blocks'.format(country_code)), 'w') as f:
+        for block in country[1]:
+            f.write(block)
+            f.write('\n')
+
+    # Write IPv6 blocks file per country
+    with open(os.path.join(DST_DIR, '{}.ipv6.blocks'.format(country_code)), 'w') as f:
+        for block in country[2]:
+            f.write(block)
+            f.write('\n')
+
+    # Write collapsed IPv4 blocks file per country
+    with open(os.path.join(DST_DIR, '{}.ipv4.collapsed'.format(country_code)), 'w') as f:
+        for block in ipaddress.collapse_addresses([ipaddress.IPv4Network(b) for b in country[1]]):
+            f.write(str(block))
+            f.write('\n')
+
+    # Write collapsed IPv6 blocks file per country
+    with open(os.path.join(DST_DIR, '{}.ipv6.collapsed'.format(country_code)), 'w') as f:
+        for block in ipaddress.collapse_addresses([ipaddress.IPv6Network(b) for b in country[2]]):
+            f.write(str(block))
+            f.write('\n')
